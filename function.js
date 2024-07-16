463f16d17ddb161f16f9ad376364306fb29f4654
fabio <Simon Bao>
2024-07-16 02:44:39 -0300

    .

---
+++ function.js
@@ -0,0 +1,27 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb){
+  var result = "";
+  result+="The " + myAdjective +" "+ myNoun +" " + myVerb + " to the store " + myAdverb; 
+  return result;
+}
+
+// console.log(wordBlanks("dog","big","ran","quickly"));
+// console.log(wordBlanks("bicycle","small","enter","quickly"));
+
+var ourArray = ["John",23];
+
+var myArray = ["Quincy",1];
+
+var onArray = [["the universe",42],["everything",101010]];
+
+var myArray = [["Bulls",23],["White sox", 45]]
+
+var ourArray = [50,60,70];
+var ourData = ourArray[0];
+
+ourData[1] = 45;
+
+onArray_item = onArray[0][0];
+console.log(onArray_item);
+
+onArray.push(["Michael","Jackson"]);
+console.log(onArray);
---
+++ tutorial-java.js
@@ -0,0 +1,19 @@
+
+
+var myName = "Beau"
+
+myName = 8
+
+let ourName = "freeCodeCamp"
+
+const pi = 3.14  //Can never be changed (it would prompt an error)
+
+
+var a; //recommended to put semicolon
+
+var firstName = "Ada";
+
+var firstNameLength = firstName[1];
+var lastNameLength = firstName[firstName.length-1];
+
+console.log(firstNameLength);
